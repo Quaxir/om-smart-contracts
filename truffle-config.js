ccb66cab1c4af2dedfab502e71f5fec512e8d309
Antonio Antonino <antonio.antonino@ericsson.com>
2020-06-26 12:25:19 +0300

    Update npm scripts + blockchain Docker start script

--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,12 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "^0.5.0"
+      version: "^0.5.0",
+      settings: {
+        optimizer: {
+          enabled: true
+        }
+      }
     }
   }
 }
